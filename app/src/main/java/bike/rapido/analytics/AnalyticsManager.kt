f7c353df1ef36a817b1b438c6fd47b2fb41b787b
adnan-rapido <adnan.am@ally.rapido.bike>
2022-07-29 17:17:03 +0530

    Added validations & data constraints

--- app/src/main/java/bike/rapido/analytics/AnalyticsManager.kt
+++ app/src/main/java/bike/rapido/analytics/AnalyticsManager.kt
@@ -3,7 +3,6 @@ package bike.rapido.analytics
 object AnalyticsManager {
 
     private val trackerList = listOf(AnalyticTrackers.CLEVERTAP, AnalyticTrackers.FIREBASE, AnalyticTrackers.APPSFLYER)
-
     private fun sendEventToCleverTap() {
         val formattedData = formatDataForClevertap()
         // RapidoAnalytics.send(Tracker.CleverTap)
@@ -25,7 +24,7 @@ object AnalyticsManager {
         } else if (trackers.contains(AnalyticTrackers.CLEVERTAP)) {
             sendEventToCleverTap()
         } else if (trackers.contains(AnalyticTrackers.FIREBASE)) {
-            sendEventToCleverTap()
+            sendEventToFirebase()
         } else {
             sendEventToAppsFlyer()
         }
@@ -35,15 +34,18 @@ object AnalyticsManager {
         return "Some formatted data"
     }
 
+    private fun validateData(name: String, pinCode: Int, city: String) {
+        // Check for validations & throw exceptions here!
+    }
 
-    public fun sendSplashScreenEvent(name: String, pinCode: Int, city: String) {
+    fun sendSplashScreenEvent(name: String, pinCode: Int, city: String) {
+        validateData(name, pinCode, city); // Useful to validate data constraints
         sendEventToAll(data = name);
     }
 
-    public fun sendEvent(trackers: List<AnalyticTrackers>, eventName: String, data: String) {
+    fun sendEvent(trackers: List<AnalyticTrackers>, eventName: String, data: String) {
         sendEventToAll(trackers, data)
     }
-
 }
 
 enum class AnalyticTrackers  {
